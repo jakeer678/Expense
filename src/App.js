d70c0a447be4f2a99d9fea28ed8385ac7c5f984d
jakeer678 <jakeersasanoor@gmail.com>
2023-03-01 20:05:06 +0530

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
-import React from 'react'
-import ExpenseHeder from './components/ExpenseHeder'
-
+import React from "react";
+import ExpenseItem from "./components/ExpenseItem";
 
 const App = () => {
-  return (
-    <div>
-      <ExpenseHeder />
-    </div>
-  )
-}
+  const expense = [
+    {
+      id: 1,
+      title: "Toilet paper",
+      amount: 94.12,
+      date: new Date(2021, 2, 28),
+      location: "Bengaluru",
+    },
+    {
+      id: 2,
+      title: "Car Insurance",
+      amount: 100,
+      date: new Date(2021, 2, 28),
+      location: "pune",
+    },
+    {
+      id: 3,
+      title: "New Tv",
+      amount: 50,
+      date: new Date(2021, 2, 28),
+      location: "Hyderbad",
+    },
+    {
+      id: 4,
+      title: "New Bike",
+      amount: 60,
+      date: new Date(2021, 2, 28),
+      location: "Delhi",
+    },
+  ];
 
-export default App
+  return <div>{
+    expense.map(item=>(
+      <ExpenseItem key={item.id} id={item.id} title={item.title} amount={item.amount} date={item.date} location={item.location} />
+    ))
+  }</div>;
+};
+
+export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./ExpenseItem.css";
+
+const ExpenseItem = (props) => {
+
+
+  return (
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item_description">
+        <h2>{props.title}</h2>
+        <div className="expense-item_price">${props.amount}</div>
+        <div>{props.location}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseItem;
+                     
