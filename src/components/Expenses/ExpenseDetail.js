7ff0cdbb2d9ae126c080674c8bc3f64d4ed8468a
jakeer678 <jakeersasanoor@gmail.com>
2023-03-02 13:08:27 +0530

    updated

--- src/components/Expenses/ExpenseDetail.js
+++ src/components/Expenses/ExpenseDetail.js
@@ -1,11 +1,21 @@
 import React from "react";
 
 const ExpenseDetail = (props) => {
+  function deleteExepense() {
+    const deleteExpense = document.getElementById("titleExpense");
+    deleteExpense.remove();
+  }
+
   return (
     <div>
       <div className="expense-item_description">
         <h2>{props.title}</h2>
-        <div className="expense-item_price">${props.amount}</div>
+        <div id="titleExpense" className="expense-item_price">
+          ${props.amount}
+        </div>
+        <div onClick={deleteExepense} className="delete">
+          Delete
+        </div>
       </div>
     </div>
   );
