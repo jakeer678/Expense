37b868bdc23963a6cc1f0c78ea01617dfd1db3d3
jakeer678 <jakeersasanoor@gmail.com>
2023-03-05 18:35:28 +0530

    updated

--- src/components/Expenses/ExpenseForm.js
+++ src/components/Expenses/ExpenseForm.js
@@ -4,6 +4,7 @@ const ExpenseForm = (props) => {
   const [enterTitle, setEnterTitle] = useState("");
   const [enterAmount, setEnterAmount] = useState("");
   const [enterDate, setEnterDate] = useState("");
+  const [show, setShow] = useState(true);
 
   function handleExpense(event) {
     setEnterTitle(event.target.value);
@@ -30,41 +31,60 @@ const ExpenseForm = (props) => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="new-expense_controls">
-        <div className="new-expense_control">
-          <label>Title</label>
-          <input
-            type="text"
-            onChange={handleExpense}
-            placeholder="Enter expense"
-            value={enterTitle}
-          />
+    <>
+      {show ? (
+        <form onSubmit={handleSubmit}>
+          <div className="new-expense_controls">
+            <div className="new-expense_control">
+              <label>Title</label>
+              <input
+                type="text"
+                onChange={handleExpense}
+                placeholder="Enter expense"
+                value={enterTitle}
+              />
+            </div>
+            <div className="new-expense_control">
+              <label>Amount</label>
+              <input
+                type="number"
+                placeholder="Enter Amount"
+                onChange={handleAmount}
+                value={enterAmount}
+              />
+            </div>
+            <div className="new-expense_control">
+              <label>Date</label>
+              <input
+                type="date"
+                min="2019-02-01"
+                max="2-03-2023"
+                onChange={handleDate}
+                value={enterDate}
+              />
+            </div>
+            <div className="new-expense_actions">
+              <button onClick={() => setShow(true)} type="submit">
+                cancel
+              </button>
+            </div>
+            <div className="new-expense_actions">
+              <button onClick={() => setShow(false)} type="submit">
+                Add new Expense
+              </button>
+            </div>
+          </div>
+        </form>
+      ) : (
+        <div className="new-expense_controls_tog">
+          <div className="new-expense_actions">
+            <button onClick={() => setShow(true)} type="submit">
+              Add new Expense
+            </button>
+          </div>
         </div>
-        <div className="new-expense_control">
-          <label>Amount</label>
-          <input
-            type="number"
-            placeholder="Enter Amount"
-            onChange={handleAmount}
-            value={enterAmount}
-          />
-        </div>
-        <div className="new-expense_control">
-          <label>Date</label>
-          <input
-            type="date"
-            min="2019-02-01"
-            max="2-03-2023"
-            onChange={handleDate}
-            value={enterDate}
-          />
-        </div>
-        <div className="new-expense_actions">
-          <button type="submit">Add Expense</button>
-        </div>
-      </div>
-    </form>
+      )}
+    </>
   );
 };
 
